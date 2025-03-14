c0931e2e7358948b5c71c3bc14acae69c081dab3
Uwe L. Korn <uwe.korn@quantco.com>
2025-03-14 14:29:46 +0100

    MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.13.20.26.56

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -54,7 +54,7 @@ ulimit -n 1024
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y xorg-x11-server-Xorg
+/usr/bin/sudo -n yum install -y xorg-x11-server-xorg
 )
 
 # make the build number clobber
